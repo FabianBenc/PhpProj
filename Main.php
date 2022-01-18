b374e869338cdf497c94a5f3b1b67bdb4569e547
Fabian Benc <bencfabian@hotmail.com>
2022-01-18 18:20:10 +0100

    Finished pagination

--- Main.php
+++ Main.php
@@ -129,7 +129,7 @@ opacity: .95;
 
   $result_count = mysqli_query(
     $link,
-    "SELECT COUNT(*) As total_records FROM `patients`"
+    "SELECT COUNT(*) As total_records FROM `patients` WHERE {$_SESSION['id']} = id"
     );
     $total_records = mysqli_fetch_array($result_count);
     $total_records = $total_records['total_records'];
@@ -182,14 +182,35 @@ opacity: .95;
   }
 mysqli_close($link);
 //session_start();
-$NAME=$_SESSION['id'];
-
-echo "Name is ".$NAME;
-echo $_SESSION['id'];
+//$NAME=$_SESSION['id'];
+//echo "Name is ".$NAME;
+//echo $_SESSION['id'];
 ?>
-</div>
 <div style='padding: 10px 20px 0px; border-top: dotted 2px #CCC; margin-left:390px; max-width:74%;'>
     <strong>Page <?php echo $page_no." of ".$total_no_of_pages; ?></strong>
+<ul class="pagination">
+<?php if($page_no > 1){
+echo "<li class='page-item'><a class='page-link' href='?page_no=1'>First Page</a></li>";
+} ?>
+    
+<li class='page-item' <?php if($page_no <= 1){ echo "class='disabled'"; } ?>>
+<a class='page-link'<?php if($page_no > 1){
+echo "href='?page_no=$previous_page'";
+} ?>>Previous</a>
+</li>
+    
+<li class='page-item'<?php if($page_no >= $total_no_of_pages){
+echo "class='disabled'";
+} ?>>
+<a class='page-link'<?php if($page_no < $total_no_of_pages) {
+echo "href='?page_no=$next_page'";
+} ?>>Next</a>
+</li>
+
+<?php if($page_no < $total_no_of_pages){
+echo "<li class='page-item'><a class='page-link' href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+} ?>
+</ul>
 </div>
 <script>
   $(document).ready(function($) {
